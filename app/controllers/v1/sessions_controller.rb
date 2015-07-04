7352c72138eba680e5c19f77d00505f02c87b61d
miyohide <miyohide@gmail.com>
2015-07-04 18:58:39 +0900

    fix don't read user parameter

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -3,12 +3,12 @@ module V1
     skip_before_action :authenticate_user_from_token!
 
     def create
-      @user = User.find_for_database_authentication(email: params[:username])
+      @user = User.find_for_database_authentication(email: params[:user][:email])
       return invalid_login_attempt unless @user
 
-      if @user.valid_password?(params[:password])
+      if @user.valid_password?(params[:user][:password])
         sign_in :user, @user
-        render json: @user, serializer: SessionSerializer, root: nil
+        render json: @user, serializer: SessionSerializer, root: false
       else
         invalid_login_attempt
       end
