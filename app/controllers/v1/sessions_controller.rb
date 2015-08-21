707dd395b0c3aa97bf197a4a95d27780588f2383
miyohide <miyohide@gmail.com>
2015-08-21 22:49:00 +0900

    fix a way to get the data from param

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -3,10 +3,10 @@ module V1
     skip_before_action :authenticate_user_from_token!
 
     def create
-      @user = User.find_for_database_authentication(email: params[:user][:email])
+      @user = User.find_for_database_authentication(email: params[:email])
       return invalid_login_attempt unless @user
 
-      if @user.valid_password?(params[:user][:password])
+      if @user.valid_password?(params[:password])
         sign_in :user, @user
         render json: @user, serializer: SessionSerializer, root: false
       else
